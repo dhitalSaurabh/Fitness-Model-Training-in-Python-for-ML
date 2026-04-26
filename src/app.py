ba26bb52fa235cc8590410098095251801bc2980
Saurabh <dhitalsaurabh@gmail.com>
2026-04-26 12:45:27 +0545

    added bug fixes to deploy

--- src/app.py
+++ src/app.py
@@ -485,5 +485,10 @@ def home():
     return jsonify({"status": "Flask ML API running — TDEE + Fitness Recommendation models active"})
 
 
+# if __name__ == "__main__":
+#     app.run(host="127.0.0.1", port=5000, debug=True)
+  
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)  
+    
